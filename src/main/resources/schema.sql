23ff0df4b4ba261e61adb60aca0b2587b7b663c3
tonchan <cleartone1216@gmail.com>
2021-10-23 23:37:04 +0900

    typo

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS todo (
-    todo_id BIGINT INDENTITY PRIMARY KEY,
+    todo_id BIGINT IDENTITY PRIMARY KEY,
     todo_title VARCHAR(30),
     finished BOOLEAN,
     created_at TIMESTAMP
